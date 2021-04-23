3ea5b3cfa59f0f25fd6e67b81839594525f9e0ad
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-04-23 16:58:12 +0530

    removed main

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,7 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-    return (
-        <div id="main">
-            <p>I am learning React. My life is getting better.</p>
-        </div>
-    );
+    return <p>I am learning React. My life is getting better.</p>;
 };
 
 export default App;
